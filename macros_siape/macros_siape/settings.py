0547d7a60c1331f93fc025d5fc6c66013b69241f
¨Igor <muniz.igor@gmail.com>
2020-06-12 23:54:30 -0300

    Atualizando static_root

--- macros_siape/macros_siape/settings.py
+++ macros_siape/macros_siape/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "static/"))
+STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "assets/"))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
